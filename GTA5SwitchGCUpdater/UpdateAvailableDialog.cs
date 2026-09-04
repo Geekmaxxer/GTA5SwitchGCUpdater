79adc70f5736b3821bb5f3bec5051a234fbdc318
Pew <83255316+Geekmaxxer@users.noreply.github.com>
2026-09-04 15:43:58 -0700

    Fix update dialog

--- GTA5SwitchGCUpdater/UpdateAvailableDialog.cs
+++ GTA5SwitchGCUpdater/UpdateAvailableDialog.cs
@@ -20,7 +20,7 @@ namespace GTA5SwitchGCUpdater
             MinimizeBox = false;
             ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterParent;
-            ClientSize = new Size(380, 168);
+            ClientSize = new Size(380, 170);
 
             var iconBox = new PictureBox
             {
@@ -39,34 +39,35 @@ namespace GTA5SwitchGCUpdater
 
             var currentVersionLabel = new Label
             {
-                Text = $"Current version:   {currentVersion}",
+                Text = $"Current version: {currentVersion}",
                 AutoSize = true,
                 Location = new Point(64, 56)
             };
 
             var newVersionLabel = new Label
             {
-                Text = $"New version:        {latestVersion}",
+                Text = $"New version: {latestVersion}",
                 AutoSize = true,
                 Font = new Font(Font, FontStyle.Bold),
                 Location = new Point(64, 76)
+            };
+
+            var goButton = new Button
+            {
+                Text = "Take me there",
+                Size = new Size(110, 32),
+                Location = new Point(80, 120),
+                UseVisualStyleBackColor = true
             };
 
             var dismissButton = new Button
             {
                 Text = "I'm good",
                 Size = new Size(110, 32),
-                Location = new Point(150, 120),
+                Location = new Point(198, 120),
                 DialogResult = DialogResult.Cancel
             };
 
-            var goButton = new Button
-            {
-                Text = "Take me there",
-                Size = new Size(130, 32),
-                Location = new Point(268, 120),
-                UseVisualStyleBackColor = true
-            };
             goButton.Click += GoButton_Click;
 
             AcceptButton = goButton;
